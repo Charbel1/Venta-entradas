8f80c16744ea45e2fb595c80acdbb230b0af9171
charbel <charbelnacharn@gmail.com>
2018-09-12 21:06:11 -0400

    .

--- front/login/app/components/dashboard/dashboard.js
+++ front/login/app/components/dashboard/dashboard.js
@@ -41,9 +41,13 @@ angular.module('app.dashboard', ['ngRoute', 'checklist-model', 'LocalStorageModu
       $scope.aux = 'main'
       $scope.aux2 = false
       $scope.auxerror = false
-      $scope.si = false;
+      $scope.si = false
 
-      $scope.headersCsv =['Cerradas', 'Abiertas', 'Alarma', 'No Data']
+
+
+
+
+      $scope.headersCsv = ['Nombre', 'Apellido', 'Cédula', 'Correo', 'Fecha', 'Código', 'Código Entrada', 'Entregada']
       $scope.cambio = function() {
 
         console.log($scope.cable)
@@ -72,203 +76,206 @@ angular.module('app.dashboard', ['ngRoute', 'checklist-model', 'LocalStorageModu
       }
 
 
-      $scope.getArray = function(market) {
-
-        var data = {}
-        data.token = tokenapp
-        data.market = market
-        data.sinceP = $scope.period1
-        data.user = userd
-
-
-
-
-        var a = request.post(ip + '/farmatodo/GetTime', data, {
-            'Content-Type': 'application/x-www-form-urlencoded'
-          })
-          .then(function(res) {
-
-            $scope.array = res.data.data
-            return res.data.data
-            console.log(res.data.data)
-
-          }, function(errorMsg) {
-            if (errorMsg.data != null) {
-
-
-
-            } else {
-              console.log("exploto get time")
-            }
-          });
-        return a
-
-      }
-
-
-
-$scope.renviarC = function (v)
+$scope.getFecha = function()
 {
-  var data = {}
-  data.cedula = v
-console.log(data)
-  $scope.auxerror = false
-  request.post(ip + '/wango/renviarC', data, {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    })
-    .then(function(res) {
 
-      console.log(res)
-      if (res.data.error != 0) {
-
-        $scope.auxerror = true
-       $scope.error = res.data.error
-      } else {
-
-        $scope.auxerror = true
-          $scope.error ="Ok"
-
-    }
-  }, function(errorMsg) {
-      $scope.auxerror = true
-      $scope.error = "Error de conexión"
-
-    });
-
+  var date = new Date();
+  var a =date.getFullYear() + '-' + ('0' + (date.getMonth() + 1)).slice(-2) + '-' + ('0' + date.getDate()).slice(-2)
+  return a+'.csv'
 }
 
 
 
-      $scope.reporte = function ()
-      {
-        var data = {}
-        $scope.auxerror = false
-        request.post(ip + '/wango/reporte', data, {
-            'Content-Type': 'application/x-www-form-urlencoded'
-          })
-          .then(function(res) {
+    $scope.getArray = function() {
 
-            console.log(res)
-            if (res.data.error != 0) {
+      var data = {}
+      data.token = ""
 
-              $scope.auxerror = true
+
+      var a = request.post(ip + '/wango/reportecsv', data, {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        })
+        .then(function(res) {
+          console.log(res.data.data)
+          $scope.array = res.data.data
+          return res.data.data
+          console.log(res.data.data)
+
+        }, function(errorMsg) {
+          $scope.auxerror = true
+          $scope.error = "Error de conexión"
+
+        });
+      return a
+
+    }
+
+
+
+    $scope.renviarC = function(v) {
+      var data = {}
+      data.cedula = v
+      console.log(data)
+      $scope.auxerror = false
+      request.post(ip + '/wango/renviarC', data, {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        })
+        .then(function(res) {
+
+          console.log(res)
+          if (res.data.error != 0) {
+
+            $scope.auxerror = true
+            $scope.error = res.data.error
+          } else {
+
+            $scope.auxerror = true
+            $scope.error = "Ok"
+
+          }
+        }, function(errorMsg) {
+          $scope.auxerror = true
+          $scope.error = "Error de conexión"
+
+        });
+
+    }
+
+
+
+    $scope.reporte = function() {
+      var data = {}
+      $scope.auxerror = false
+      request.post(ip + '/wango/reporte', data, {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        })
+        .then(function(res) {
+
+          console.log(res)
+          if (res.data.error != 0) {
+
+            $scope.auxerror = true
             //  $scope.error = res.data.error
-            } else {
+          } else {
 
-              $scope.auxerror = true
-                $scope.error = "No Entregadas "+res.data.data.no + " Entregadas "+res.data.data.entregada
-            }
-
-          }, function(errorMsg) {
             $scope.auxerror = true
-            $scope.error = "Error de conexión"
+            $scope.error = "No Entregadas " + res.data.data.no + " Entregadas " + res.data.data.entregada
+          }
 
-          });
+        }, function(errorMsg) {
+          $scope.auxerror = true
+          $scope.error = "Error de conexión"
 
-      }
+        });
 
-      $scope.cambiar = function(algo) {
-        $scope.auxerror = false
-        $scope.aux2 = false
-        $scope.si = false
-        $scope.aux = algo;
-        $scope.form = {}
-        $scope.form.cedula = "";
-        $scope.form.correo = "";
-        $scope.form.apellido = "";
-        $scope.form.nombre = "";
-        $scope.entrada = ""
-        $scope.cedula=""
+    }
 
-      }
+    $scope.cambiar = function(algo) {
+      $scope.auxerror = false
+      $scope.aux2 = false
+      $scope.si = false
+      $scope.aux = algo;
+      $scope.form = {}
+      $scope.form.cedula = "";
+      $scope.form.correo = "";
+      $scope.form.apellido = "";
+      $scope.form.nombre = "";
+      $scope.entrada = ""
+      $scope.cedula = ""
 
-      $scope.buscar = function(cedula) {
-        $scope.aux2 = false
-        $scope.si = false
+    }
 
-        var data = {}
+    $scope.buscar = function(cedula) {
+      $scope.aux2 = false
+      $scope.si = false
 
-        data.cedula = cedula
+      var data = {}
 
-        request.post(ip + '/wango/buscarC', data, {
-            'Content-Type': 'application/x-www-form-urlencoded'
-          })
-          .then(function(res) {
+      data.cedula = cedula
 
-            if (res.data.error != 0) {
+      request.post(ip + '/wango/buscarC', data, {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        })
+        .then(function(res) {
 
-              $scope.auxerror = true
-              $scope.error = res.data.error
-            } else {
-              $scope.aux2 = true
-              $scope.auxerror = false
-              console.log(res.data.data)
-              $scope.data = res.data.data
-              $scope.si = true;
-            }
+          if (res.data.error != 0) {
 
-          }, function(errorMsg) {
             $scope.auxerror = true
-            $scope.error = "Error de conexión"
+            $scope.error = res.data.error
+          } else {
+            $scope.aux2 = true
+            $scope.auxerror = false
+            console.log(res.data.data)
+            $scope.data = res.data.data
+            $scope.si = true;
+          }
 
-          });
+        }, function(errorMsg) {
+          $scope.auxerror = true
+          $scope.error = "Error de conexión"
+
+        });
 
 
-      }
+    }
 
 
-      $scope.aprobar = function(cedula,entrada) {
-        $scope.auxerror = false
-        $scope.aux2 = false
-        $scope.si = false
+    $scope.aprobar = function(cedula, entrada) {
+      $scope.auxerror = false
+      $scope.aux2 = false
+      $scope.si = false
 
-        var data = {}
-        data.cedula=cedula
-        data.entrada = entrada
+      var data = {}
+      data.cedula = cedula
+      data.entrada = entrada
 
-        console.log(data)
+      console.log(data)
 
-        request.post(ip + '/wango/aprobar', data, {
-            'Content-Type': 'application/x-www-form-urlencoded'
-          })
-          .then(function(res) {
-            console.log(res)
-            if (res.data.error != 0) {
-              $scope.auxerror = true
-              $scope.error = res.data.error
-            } else {
-              $scope.form = {}
-              $scope.form.cedula = "";
-              $scope.form.correo = "";
-              $scope.form.apellido = "";
-              $scope.form.nombre = "";
-              $scope.entrada = ""
-              $scope.cedula=""
-                $scope.aux = 'main';
-            }
-
-          }, function(errorMsg) {
+      request.post(ip + '/wango/aprobar', data, {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        })
+        .then(function(res) {
+          console.log(res)
+          if (res.data.error != 0) {
             $scope.auxerror = true
-            $scope.error = "Error de conexión"
+            $scope.error = res.data.error
+            $scope.entrada = "";
 
-          });
+          } else {
+            $scope.form = {}
+            $scope.form.cedula = "";
+            $scope.form.correo = "";
+            $scope.form.apellido = "";
+            $scope.form.nombre = "";
+            $scope.entrada = ""
+            $scope.cedula = ""
+            $scope.aux = 'main';
+          }
+
+        }, function(errorMsg) {
+          $scope.auxerror = true
+          $scope.data = ""
+          $scope.entrada = "";
+          $scope.error = "Error de conexión"
+
+        });
 
 
 
-      }
-      var emailValid = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i
-      // email.match(emailValid)
+    }
+    var emailValid = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i
+    // email.match(emailValid)
 
-      $scope.enviar = function() {
-        $scope.auxerror = false
-        var data = $scope.form
+    $scope.enviar = function() {
+      $scope.auxerror = false
+      var data = $scope.form
 
-        var validCorreo = $scope.form.correo.match(emailValid)
-        console.log(validCorreo)
-        if (validCorreo != null) {
-          if ($scope.form.cedula.length > 0) {
-            if ($scope.form.nombre.length > 0) {
-                if ($scope.form.apellido.length > 0) {
+      var validCorreo = $scope.form.correo.match(emailValid)
+      console.log(validCorreo)
+      if (validCorreo != null) {
+        if ($scope.form.cedula.length > 0) {
+          if ($scope.form.nombre.length > 0) {
+            if ($scope.form.apellido.length > 0) {
               console.log("entro")
               request.post(ip + '/wango/registro', data, {
                   'Content-Type': 'application/x-www-form-urlencoded'
@@ -308,27 +315,26 @@ console.log(data)
 
         }
 
-        } else {
-          $scope.auxerror = true
-          $scope.error = "No es un correo valido"
-        }
-
-
-
-
-
+      } else {
+        $scope.auxerror = true
+        $scope.error = "No es un correo valido"
       }
 
 
 
 
 
-
-
-
-
-      $scope.bool = true;
-
-
     }
-  ]);
+
+
+
+
+
+
+
+
+
+    $scope.bool = true;
+
+
+  }]);
