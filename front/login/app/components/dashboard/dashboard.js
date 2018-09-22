17ec3ad260c6f7ac2d5b509e458a3f5bd92b6fee
root <root@wango>
2018-09-22 16:37:09 -0400

    cambio mensaje

--- front/login/app/components/dashboard/dashboard.js
+++ front/login/app/components/dashboard/dashboard.js
@@ -164,7 +164,7 @@ angular.module('app.dashboard', ['ngRoute', 'checklist-model', 'LocalStorageModu
             } else {
 
               $scope.auxerror = true
-              $scope.error = "No Entregadas " + res.data.data.no + " Entregadas " + res.data.data.entregada
+              $scope.error = "No Entregadas " + res.data.data.no + " Vendidas " + res.data.data.entregada
             }
 
           }, function(errorMsg) {
